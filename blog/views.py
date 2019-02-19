ea5906caa09161ba4dab4ccde754efadeb4614e4
Theethawat <5910110150@psu.ac.th>
2019-02-19 21:32:57 +0700

    modify view and template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
+# The Dot Before is show that it's the same directory
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-     return render(request,'blog/post_list.html',{})
+     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    #{}, is a place in which we can add some things for the template to use
+     return render(request,'blog/post_list.html',{'posts':posts})
