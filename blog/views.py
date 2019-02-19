11b7750e5b58b3b6cefece059ca9d531b5067608
Theethawat <5910110150@psu.ac.th>
2019-02-19 22:18:50 +0700

    add postdetail in view.py linked but not design HTML Template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # The Dot Before is show that it's the same directory
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,7 @@ def post_list(request):
      posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     #{}, is a place in which we can add some things for the template to use
      return render(request,'blog/post_list.html',{'posts':posts})
+     
+def post_detail(request,pk):
+     post = get_object_or_404(Post, pk=pk)
+     return render(request, 'blog/post_detail.html',{'post':post})
